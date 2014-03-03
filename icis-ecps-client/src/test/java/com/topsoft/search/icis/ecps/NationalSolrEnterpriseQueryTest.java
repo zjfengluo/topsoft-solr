52316cc58cbdbe882d4769ef7db6a760c46b5490
weichao <whtmlb@gmail.com>
2014-03-03 11:38:57 +0800

    update unit test

--- icis-ecps-client/src/test/java/com/topsoft/search/icis/ecps/NationalSolrEnterpriseQueryTest.java
+++ icis-ecps-client/src/test/java/com/topsoft/search/icis/ecps/NationalSolrEnterpriseQueryTest.java
@@ -1,14 +1,24 @@
 package com.topsoft.search.icis.ecps;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrInputDocument;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import com.topsoft.search.HttpSolrServerCache;
 
@@ -18,14 +28,26 @@ import com.topsoft.search.HttpSolrServerCache;
  * @author weichao
  * 
  */
+@RunWith(JUnit4.class)
 public class NationalSolrEnterpriseQueryTest {
 	
-	private static final String baseURL = "http://192.168.3.40:9081/solr";
+	public static final String baseURL = "http://192.168.3.40:8983/solr";
 	
 	private NationalSolrEnterpriseQuery query = new NationalSolrEnterpriseQuery(baseURL);
+	
+	public static final String keyword = "好运来";
+	
+	public static final String regNo = "9999999";
+	
+	public static final String opLocDistrict = "410000";
+	
+	public static final String entType = "9600";
+	
+	public static final String industryPhy = "C";
 
+	@Test
 	public void testHighlight() {
-		SolrQuery query = new SolrQuery("entName:" + "好运来");
+		SolrQuery query = new SolrQuery("entName:" + keyword);
 		query.setStart(0);
 		query.setRows(5);
 		query.setHighlight(true);
@@ -40,19 +62,50 @@ public class NationalSolrEnterpriseQueryTest {
 			SolrDocument sd = solrDocuments.iterator().next();
 			String id = sd.getFieldValue("id").toString();
 			Map<String, List<String>> map = res.getHighlighting().get(id);
-			assert map.size() == 1 : "================== error when testing testHighlight ...";
-			System.out.println("================== testHighlight testing  ok ...");
+			assertTrue(map.size() == 1);
 		} catch (SolrServerException e) {
 			throw new RuntimeException(e);
 		}
 	}
 	
+	@Test
 	public void testFind() {
-		List<NationalEntBaseInfo> list = query.find("410823620003885");
-		assert list.size() == 1 : "================== error when testing testFind regNo ...";
-		System.out.println("================== testFind regNo testing ok ...");
-		list = query.find("好运来");
-		assert list.size() > 1 : "================== error when testing testFind entName ...";
-		System.out.println("================== testFind entName testing ok ...");
+		List<NationalEntBaseInfo> list = query.find(regNo);
+		assertTrue(list.size() == 1);
+		list = query.find(keyword);
+		assertTrue(list.size() >= 1);
+	}
+	
+	@BeforeClass	
+	public static void prepare() throws SolrServerException, IOException {
+		addDoc();
+	}
+	
+	@AfterClass
+	public static void cleanup() throws SolrServerException, IOException {
+		deleteDoc();
+	}
+	
+	public static void addDoc() throws SolrServerException, IOException {
+		SolrServer solr = new HttpSolrServer(baseURL+"/entbaseinfo");
+		SolrInputDocument document = new SolrInputDocument();
+		document.addField("id", "5521991");
+		document.addField("entName", keyword);
+		document.addField("regCap", "9.99");
+		document.addField("regCapLevel", "1");
+		document.addField("domOrOpLoc", keyword);
+		document.addField("leRep", keyword);
+		document.addField("regNo", regNo);
+		document.addField("opLocDistrict", opLocDistrict);
+		document.addField("entType", entType);
+		document.addField("industryPhy", industryPhy);
+		solr.add(document);
+		solr.commit();
+	}
+	
+	public static void deleteDoc() throws SolrServerException, IOException {
+		SolrServer solr = new HttpSolrServer(baseURL+"/entbaseinfo");
+		solr.deleteById("5521991");
+		solr.commit();
 	}
 }
--- icis-ecps-client/src/test/java/com/topsoft/search/icis/ecps/TopSolrEnterpriseQueryTest.java
+++ icis-ecps-client/src/test/java/com/topsoft/search/icis/ecps/TopSolrEnterpriseQueryTest.java
@@ -1,6 +1,16 @@
 package com.topsoft.search.icis.ecps;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
 import java.util.List;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import com.topsoft.search.domain.Page;
 import com.topsoft.search.domain.PageRequest;
@@ -10,64 +20,81 @@ import com.topsoft.search.domain.Pageable;
  * 拓普企业信用信息公示系统企业查询接口 - 单元测试
  * 
  * @author weichao
- *
+ * 
  */
+@RunWith(JUnit4.class)
 public class TopSolrEnterpriseQueryTest {
 
-	private static final String baseURL = "http://192.168.3.40:9081/solr";
-	
 	private Pageable pageRequest = PageRequest.builder(1, 10).build();
+
+	private TopSolrEnterpriseQuery query = new TopSolrEnterpriseQuery(
+			NationalSolrEnterpriseQueryTest.baseURL);
+
+	@BeforeClass
+	public static void prepare() throws SolrServerException, IOException {
+		NationalSolrEnterpriseQueryTest.addDoc();
+	}
 	
-	private TopSolrEnterpriseQuery query = new TopSolrEnterpriseQuery(baseURL);
-	
-	private static final String keyword = "好运来";
-	
+	@AfterClass
+	public static void cleanup() throws SolrServerException, IOException {
+		NationalSolrEnterpriseQueryTest.deleteDoc();
+	}
+
 	/**
 	 * 测试根据企业名称进行模糊查询
 	 */
+	@Test
 	public void testFindByEntName() {
-		Page<TopEntBaseInfo> page = query.findByEntName(keyword, pageRequest);
-		assert page.getNumberOfElements() > 0 : "================== error when testing testFindByEntName ...";
-		System.out.println("================== testFindByEntName testing  ok  ... totalDoc:" + page.getTotalElements());
+		Page<TopEntBaseInfo> page = query.findByEntName(
+				NationalSolrEnterpriseQueryTest.keyword, pageRequest);
+		assertTrue(page.getNumberOfElements() > 0);
 	}
-	
+
 	/**
 	 * 测试根据注册号进行查询
 	 */
+	@Test
 	public void testFindByRegNo() {
-		List<TopEntBaseInfo> page = query.findByRegNo("410823620003885");
-		assert page.size() == 1 : "================== error when testing testFindByRegNo ...";
-		System.out.println("================== testFindByRegNo testing  ok  ...");
+		List<TopEntBaseInfo> page = query.findByRegNo(NationalSolrEnterpriseQueryTest.regNo);
+		assertTrue(page.size() == 1);
 	}
-	
+
 	/**
 	 * 测试根据法定代表人进行模糊查询
 	 */
+	@Test
 	public void testFindByLeRep() {
-		Page<TopEntBaseInfo> page = query.findByLeRep(keyword, pageRequest);
-		assert page.getNumberOfElements() > 0 : "================== error when testing testFindByLeRep ...";
-		System.out.println("================== testFindByLeRep testing  ok  ... totalDoc:" + page.getTotalElements());
+		Page<TopEntBaseInfo> page = query.findByLeRep(
+				NationalSolrEnterpriseQueryTest.keyword, pageRequest);
+		assertTrue(page.getNumberOfElements() > 0);
 	}
-	
+
 	/**
 	 * 测试根据经营地址进行模糊查询
 	 */
+	@Test
 	public void testFindByOpLoc() {
-		Page<TopEntBaseInfo> page = query.findByOpLoc(keyword, pageRequest);
-		assert page.getNumberOfElements() > 0 : "================== error when testing testFindByOpLoc ...";
-		System.out.println("================== testFindByOpLoc testing  ok ... ... totalDoc:" + page.getTotalElements());
+		Page<TopEntBaseInfo> page = query.findByOpLoc(
+				NationalSolrEnterpriseQueryTest.keyword, pageRequest);
+		assertTrue(page.getNumberOfElements() > 0);
 	}
-	
+
 	/**
 	 * 测试高级查询
 	 */
+	@Test
 	public void testAdvancedFind() {
-		Page<TopEntBaseInfo> page = query.advancedFind(keyword, null, null, null, null, pageRequest);
-		assert page.getNumberOfElements() > 1 : "================== error when testing testFindByOpLoc ...";
-		System.out.println("================== testAdvancedFind testing  ok ... totalDoc:" + page.getTotalElements());
-		page = query.advancedFind(keyword, "410823", "C", RegCapLevel.LEVEL1, new String[]{"9600"}, pageRequest);
-		assert page.getNumberOfElements() == 1 : "================== error when testing testFindByOpLoc ...";
-		System.out.println("================== testAdvancedFind testing  ok ...");
+		Page<TopEntBaseInfo> page = query.advancedFind(
+				NationalSolrEnterpriseQueryTest.keyword, null, null, null,
+				null, pageRequest);
+		assertTrue(page.getNumberOfElements() >= 1);
+		page = query.advancedFind(NationalSolrEnterpriseQueryTest.keyword,
+				NationalSolrEnterpriseQueryTest.opLocDistrict,
+				NationalSolrEnterpriseQueryTest.industryPhy,
+				RegCapLevel.LEVEL1,
+				new String[] { NationalSolrEnterpriseQueryTest.entType },
+				pageRequest);
+		assertTrue(page.getNumberOfElements() >= 1);
 	}
-	
+
 }
